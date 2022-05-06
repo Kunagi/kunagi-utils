69d4e040005f1da35b3b232800c60d6be775bbde
Witoslaw Koczewski <wi@koczewski.de>
2022-05-06 21:26:12 +0200

    build: release

--- build.clj
+++ build.clj
@@ -4,5 +4,10 @@
    [clojure.tools.build.api :as b]
 
 
+   [kunagi.build.api :as kb :refer [print-task print-done print-debug]]
    ))
 
+(defn release [{:keys []}]
+  (kb/assert-git-clean)
+  (kb/assert-deps-edn-has-no-local-deps!)
+  )
